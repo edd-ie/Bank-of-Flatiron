6bf636872d221cbad56203a9e67e0df6bc5b98b6
edd-ie <ogendoeddie@gmail.com>
2023-04-22 14:17:31 +0300

    Styling and UI

--- src/Tables.js
+++ src/Tables.js
@@ -1,3 +1,4 @@
+import './Tables.css';
 import React, {useEffect,useState} from "react";
 
 
@@ -26,6 +27,19 @@ export default function Table(){
         }
     }
 
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        let form = e.target;
+        let value = form.elements[0].value;
+
+        let filtered = originalContent.filter((item) => item.category.toLowerCase() === value.toLowerCase());
+        setFilteredContent(filtered);
+
+        form.reset()
+    }
+
+
     const displayTable = (filteredContent.length > 0 ? filteredContent : originalContent).map((row)=>{
         return (
             <tr key={row.id}> 
@@ -39,18 +53,33 @@ export default function Table(){
 
     return(
         <div>
-            <select onChange={e => handleSelect(e)}>
-                <option value="Category">All</option>
-                <option value="Income">Income</option>
-                <option value="Food">Food</option>
-                <option value="Fashion">Fashion</option>
-                <option value="Gift">Gift</option>
-                <option value="Transportation">Transportation</option>
-                <option value="Entertainment">Entertainment</option>
-                <option value="Housing">Housing</option>
-            </select>            
+            <form onSubmit={(e)=>handleSubmit(e)} className="filter">
+                <div className="filter1">                    
+                    <input type="text" placeholder="Filter by category" />
+                    <button type="submit">
+                        <span class="material-symbols-outlined">
+                            filter_alt
+                        </span>
+                    </button>
+                </div>
+                <div className="filter2">
+                    <select onChange={e => handleSelect(e)}>
+                        <option value="Category">All</option>
+                        <option value="Income">Income</option>
+                        <option value="Food">Food</option>
+                        <option value="Fashion">Fashion</option>
+                        <option value="Gift">Gift</option>
+                        <option value="Transportation">Transportation</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Housing">Housing</option>
+                    </select>
+                    <button onClick={() => setFilteredContent(originalContent)}>
+                        reset
+                    </button>
+                </div>
+            </form>
+                        
             <table id="table">
-                <caption>Transaction History</caption>
                 <thead>
                     <tr>
                         <th>DESCRIPTION</th>
